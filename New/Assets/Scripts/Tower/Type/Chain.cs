7c149bc15d57195da979e417db02b93c347905a3
sungho8 <dkdlah0830@naver.com>
2020-12-04 00:12:16 +0900

    Design Gift!
    유물 디자인 시작

---
+++ New/Assets/Scripts/Tower/Type/Chain.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject tower;
+    public GameObject effect;
+    public int BulletType;
+    public int chain;
+
+    public float timeType;
+    public float damageType;
+
+    const int STUN = 0;
+    const int BURN = 1;
+    const int SLOW = 2;
+
+    
+
+    public int attack { get; set; }
+    public float speed;
+
+    public void setTarget(GameObject _target)
+    {
+        target = _target;
+    }
+
+    private void Update()
+    {
+        if(target != null)
+        {
+            transform.Rotate(0, 0, Time.deltaTime* 2f);
+
+            Vector2 targetVec = target.transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, targetVec, speed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (target != null && other.gameObject == target)
+        {
+            GameObject temp = Instantiate(effect, target.transform.position, Quaternion.identity);
+            Enemy enemy = target.GetComponent<Enemy>();
+            temp.transform.localScale = new Vector2(0.6f, 0.6f);
+            enemy.Damaged(attack);
+
+            // 상태이상
+            if(BulletType != -1) enemy.ChangeState(BulletType, timeType, damageType);
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void SetNewBullet()
+    {
+        //GameObject temp = Instantiate(this.gameObject, transform.position, Quaternion.identity);
+        //temp.transform.localScale = new Vector2(0.5f, 0.5f);
+
+
+
+        //Bullet bul = temp.GetComponent<Bullet>();
+        //bul.target = ;
+        //bul.tower = this.gameObject;
+        //bul.attack = attack;
+    }
+}
